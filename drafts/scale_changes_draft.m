fa8fc779020e783d3624baab641a3e9feb588694
Jackie Loven <jloven@wpi.edu>
2016-02-19 05:09:01 -0500

    fixed things in matlab and recorded elapsed time for matrix generation

---
+++ drafts/scale_changes_draft.m
@@ -0,0 +1,13 @@
+%  The vector A is 5 arguments:
+%  The x, y, z dimensions of the matrix
+%  The number of samples of these matrices
+
+%  The function I need to write takes three arguments
+%  The vector A
+%  A fitting function parameter (how good that particular 4D matrix is)
+%  A number from 1 to 10 (random changes to directed changes)
+
+%  Example, vector A - 6 matrices of 3x3x5 and 1 extra parameter for material:
+A = [3, 3, 5, 1, 6]
+
+
